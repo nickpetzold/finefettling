9c54164eeb31943659675de47c28a08897153d77
Nick Petzold <nickpetzold@gmail.com>
2018-12-25 21:29:57 +0000

    LAnding page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
+  resources :pages, only: [:home]
+  # resources :cars, only: [:new, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
